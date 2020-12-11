ff20408054ea10518a9baa1cf96adbc48c07b29f
Patrick Schlipf <Patrick.Schlipf@ibm.com>
2020-12-11 20:07:11 +0100

    Initial commit

--- src/main/java/com/patrick/games/textadv/items/Potion.java
+++ src/main/java/com/patrick/games/textadv/items/Potion.java
@@ -1,31 +1,3 @@
-//*****************************************************************************
-//*
-//* FILE NAME:  Potion.java
-//*
-//* IBM Confidential
-//*
-//*  OCO Source Materials
-//*
-//*  5698-SA4
-//*
-//*  © Copyright IBM Corp. 2020
-//*
-//*   The source code for this program is not published or otherwise
-//*   divested of its trade secrets, irrespective of what has been
-//*   deposited with the U.S. Copyright Office.
-//*
-//*****************************************************************************
-//*
-//* CLASSES:
-//*            Potion   (externally documented: no )
-//*
-//* CHANGE HISTORY:
-//*
-//*   Change Date     UserID   Description
-//*   -----  -------- -------- ------------------------------------------------
-//*   y0001  28112020 psch     Initial Version
-//*
-//*****************************************************************************
 package com.patrick.games.textadv.items;
 
 import com.patrick.games.textadv.units.AbstractCharacter;
